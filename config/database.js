f1639e0a1ab7b9fa71c28cb9c1bd2042f6398479
amarHDev <henni.amar24@gmail.com>
2020-04-27 00:18:59 +0200

    setting up to push to Heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || '', { useUnifiedTopology: true, useNewUrlParser: true } , (err) => {
+mongoose.connect('mongodb+srv://admin:<admin1234>@cluster0-i3eps.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true } , (err) => {
    if (err){
        console.log(err);
    }else{

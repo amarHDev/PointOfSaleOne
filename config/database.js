4edade240d3604526d063dd1b6ca0367e584ab48
amarHDev <henni.amar24@gmail.com>
2020-04-27 15:49:39 +0200

    setting up to push to Heroku

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/posdb' || 'mongodb+srv://posdb:adminadmin@cluster0-i3eps.mongodb.net/test?retryWrites=true&w=majority' , { useUnifiedTopology: true, useNewUrlParser: true } , (err) => {
+mongoose.connect(`mongodb+srv://posdb:adminadmin@cluster0-i3eps.mongodb.net/test?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true } , (err) => {
    if (err){
        console.log(err);
+       console.log('connected eroorrrr !!! ...');
    }else{
        console.log('connected to db successfuly ...');
    }
